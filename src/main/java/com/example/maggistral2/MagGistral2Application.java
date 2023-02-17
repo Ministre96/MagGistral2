af6556d9eaac1703a8f284ef3eba48d27b0408d0
Baptiste Vansnick <b.vansnick@stag.technofuturtic.education>
2023-02-17 09:34:22 +0100

    first commit

---
+++ src/main/java/com/example/maggistral2/MagGistral2Application.java
@@ -0,0 +1,13 @@
+package com.example.maggistral2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagGistral2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagGistral2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maggistral2/MagGistral2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maggistral2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagGistral2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
